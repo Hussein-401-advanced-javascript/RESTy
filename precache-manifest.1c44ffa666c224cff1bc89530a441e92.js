e121809a68f0b5d6fdba2d3a3607733d57632a68
hussein <hussein66253@icloud.com>
2020-10-12 22:40:09 +0300

    Updates

---
+++ precache-manifest.1c44ffa666c224cff1bc89530a441e92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d2200841957df791f97e2e12b32393",
+    "url": "/RESTy/index.html"
+  },
+  {
+    "revision": "24503e2c87bce3f13365",
+    "url": "/RESTy/static/css/main.51f13387.chunk.css"
+  },
+  {
+    "revision": "cf2686d047078aa370f1",
+    "url": "/RESTy/static/js/2.9b45b137.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RESTy/static/js/2.9b45b137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24503e2c87bce3f13365",
+    "url": "/RESTy/static/js/main.9b45aabf.chunk.js"
+  },
+  {
+    "revision": "cffb6c70ef4be7c07bfe",
+    "url": "/RESTy/static/js/runtime-main.8243ec0e.js"
+  }
+]);
